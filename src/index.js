143c2bab250a89b02ef1b33c225fbd5da9eeef5b
Manav Misra <manavm@visionwebsoft.com>
2022-12-14 22:32:01 -0600

    Add starter code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const buttonsDiv = document.querySelector("#buttons")
+const displayDiv = document.querySelector("#display");
+
+function createPetCard(pet) {
+  return `
+  <section class="flex flex-col gap-y-4 border rounded-sm pb-2 max-w-xs">
+    <h2 class="text-3xl font-bold text-center py-4 text-white bg-blue-700">${pet.name}</h2>
+    <figure>
+      <img alt="" src="https://source.unsplash.com/random/900×700/?dog}" />
+      <figcaption class="py-4 flex flex-col gap-y-2 items-center italic">${pet.description}<small class="not-italic">Age: ${pet.age}</small></figcaption>
+    </figure>
+  </section>
+  `
+}
+
+function createTermCard(term) {
+  return `
+    <section class="flex flex-col gap-y-4 border rounded-sm pb-2">
+      <h2 class="text-3xl font-bold text-center py-4 text-white bg-blue-700">${term.term}</h2>
+      <p class="px-4">${term.definition}</p>
+      <footer class="flex flex-col gap-y-2 items-center"><a href="${term.url}" class="text-sky-500 underline">Source</a> <span>Category: ${term.category}</span> <span>Relevance: ${term.relevance}</span> </footer>
+    </section>
+  `;
+}
+
